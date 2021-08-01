c91ed6cd378871ae3c88d626fcc23d4becd92ced
MrWhale787 <minethwart@gmail.com>
2021-08-01 21:08:33 +0800

    re written to be fully async - faster but still needs work

--- playerFormatter.py
+++ playerFormatter.py
@@ -1,15 +1,19 @@
 #honestly this doesnt need to be a seperate library, im just trying to save main from looking like shit
+import asyncio
 import robloxAPI as rbx
 
 
-def formatTeam(team):
+async def formatTeam(team):
     fTeam = ""
     players = team.keys()
     players = list(players)
-    for i in range(len(players)):
-        fplayer = team[str(players[i])]
-        userInfo = rbx.getUserInfo(players[i])
-        userName = userInfo["name"]
+    usersInfo = await rbx.getUsersInfo(players)
+    for i in (players):
+        fplayer = team[i]
+        for userData in usersInfo:
+            if str(userData["id"]) == i:
+                userName = userData["name"]
+                break
         player = {"Name":userName,"Kills": fplayer["kills"],"Deaths": fplayer["deaths"],"Score": fplayer["score"]}
         for k in player:
              fTeam += str(k) + ': ' + str(player[k]) + ' '
--- robloxAPI.py
+++ robloxAPI.py
@@ -1,17 +1,32 @@
 #roblox API handler, will probs rewrite in future, currentlys short term solution
-
-import requests as req
+import asyncio
+import aiohttp
 import json
 
 
-def getUserInfo(UID):
-    url = f"https://users.roblox.com/v1/users/{UID}"
-    response = req.get(url)
-    output = response.json()
-    if len(output) != 7:
-        error = output[0]
-        error = error[0]
-        error = error["message"]
-        return error
-    else:
+async def getUserInfo(UID):
+    async with aiohttp.ClientSession() as session:
+        url = f"https://users.roblox.com/v1/users/{UID}"
+        response = await session.get(url)
+        output = await response.json()
+        if len(output) != 7:
+            error = output[0]
+            error = error[0]
+            error = error["message"]
+            return error
+        else:
+            return output
+
+
+
+
+async def getUsersInfo(UIDs):
+    output = []
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for UID in UIDs:
+            tasks.append(session.get(f'https://users.roblox.com/v1/users/{UID}'))
+        responses = await asyncio.gather(*tasks)
+        for response in responses:
+            output.append(await response.json())
         return output
