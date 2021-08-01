cf149b322229a11ea602fe56b53c0d7d36a1f6bc
MrWhale787 <minethwart@gmail.com>
2021-08-01 16:38:06 +0800

    + added match viewer command

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 #Main script for bot
 import discord
+import LeagueCloud as LC
+import playerFormatter as pF
 from discord.ext import commands
 
 client = commands.Bot(command_prefix = "vt!")
+LC.api_key = '2ux1zyiE5vxjkEHjcn4ZeFYlfTEufGnRG1nQTbbg'
 
 @client.event
 async def on_ready():
     print('Bot is ready')
 
-client.run('api key here')
+@client.command(aliases=['match','m'])
+async def viewE(ctx,roundID):
+    data = LC.fetchMatch(roundID)
+    RID = data[0]
+    matchData = data[1]
+    playerStats = matchData["stats"]
+    matchDetails = f'Map: {matchData["map"]}, Mode: {matchData["mode"]}, Victor: {matchData["victor"]}'
+    phantoms = playerStats["Phantoms"]
+    phantomsFormatted = pF.formatTeam(phantoms)
+    ghosts = playerStats["Ghosts"]
+    ghostsFormatted = pF.formatTeam(ghosts)
+
+    embed=discord.Embed(title=RID)
+    embed.add_field(name="Match Details", value=matchDetails , inline=False)
+    embed.add_field(name="Phantoms", value=phantomsFormatted, inline=True)
+    embed.add_field(name="Ghosts", value=ghostsFormatted, inline=True)
+    await ctx.send(embed=embed)
+
+
+
+
+client.run('ODcxMjc3NjExMjYxNzg4MTcw.YQY-gg.k_PNrzt5NvIkraKNU5pfuHavMj4')
