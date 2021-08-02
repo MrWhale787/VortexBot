0a501cd8363033c92a6deaea641cc54ee3fcfcef
MrWhale787 <minethwart@gmail.com>
2021-08-02 15:23:26 +0800

    *adjusted LeagueCloud.py to be async

--- LeagueCloud.py
+++ LeagueCloud.py
@@ -1,19 +1,19 @@
-#pip install requests
 
-import requests as req
-import json
+import asyncio
+import aiohttp
 
 api_key = ""
 
-def fetchMatch(RID):
+async def fetchMatch(RID):
     if len(api_key) != 40:
         return "invalid api key"
     try:
         int(RID)
         url = f"https://api.leaguecloud.org/sclbuild/getround?id={RID}&api_key={api_key}"
         try:
-            response = req.get(url)
-            output = response.json()
+            async with aiohttp.ClientSession() as session:
+                response = await session.get(url)
+                output = await response.json()
             if output["error"] != False:
                 return output["message"]
             else:
