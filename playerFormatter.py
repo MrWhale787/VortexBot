3c66d43f2e0c334132424ca244c0f4afff19e217
MrWhale787 <minethwart@gmail.com>
2021-08-03 13:30:30 +0800

    * fixed a bug which caused no api response

--- playerFormatter.py
+++ playerFormatter.py
@@ -5,21 +5,24 @@ import robloxAPI as rbx
 
 async def formatTeam(team):
     fTeam = ""
-    players = team.keys()
-    players = list(players)
-    usersInfo = await rbx.getUsersInfo(players)
-    LB = []
-    for i in (players):
-        fplayer = team[i]
-        for userData in usersInfo:
-            if str(userData["id"]) == i:
-                userName = userData["name"]
-                break
-        player = {"Name":userName,"Kills": fplayer["kills"],"Deaths": fplayer["deaths"],"Score": fplayer["score"]}
-        LB.append(player.copy())
-    LBSorted = sorted(LB, key=lambda k: k['Score'],reverse = True)
-    for i in LBSorted:
-        for k in i:
-             fTeam += str(k) + ': ' + str(i[k]) + ' '
-        fTeam += '\n'
-    return fTeam
+    try:
+        players = team.keys()
+        players = list(players)
+        usersInfo = await rbx.getUsersInfo(players)
+        LB = []
+        for i in (players):
+            fplayer = team[i]
+            for userData in usersInfo:
+                if str(userData["id"]) == i:
+                    userName = userData["name"]
+                    break
+            player = {"Name":userName,"Kills": fplayer["kills"],"Deaths": fplayer["deaths"],"Score": fplayer["score"]}
+            LB.append(player.copy())
+        LBSorted = sorted(LB, key=lambda k: k['Score'],reverse = True)
+        for i in LBSorted:
+            for k in i:
+                 fTeam += str(k) + ': ' + str(i[k]) + ' '
+            fTeam += '\n'
+        return fTeam
+    except:
+        return "Empty"
